ed434732f18106479c7cbec162c4793e09a62d48
Chris Born <chris@peltedvenom.com>
2016-08-22 08:32:54 -0700

    refactoring for a bit more flexibility

--- assets/plugin.js
+++ assets/plugin.js
@@ -1,6 +1,73 @@
-// require(['gitbook'], function (gitbook) {
-//   gitbook.events.bind('start', function (e, config) {
-//     pluginConfig = config || {};
-//     console.log(pluginConfig);
-//   });
-// });
+'use strict';
+
+var MarkupIt = require( 'markup-it' );
+var markdownSyntax = require( 'markup-it/syntaxes/markdown' );
+var htmlSyntax = require( 'markup-it/syntaxes/html' );
+
+var markdown = new MarkupIt( markdownSyntax );
+var html = new MarkupIt( htmlSyntax );
+
+var options = {};
+var figures = [];
+
+function blockFilter( imageItem ) {
+  var image = imageItem.trim()
+  return image.length > 1;
+}
+
+function parseMarkdown( text ) {
+  md = markdown.toContent( text );
+  parsed = html.toText( md );
+  return parsed;
+};
+
+function buildFigures( block ) {
+  let blockText, images = [];
+
+  blockText = block.body.split( /\r?\n/ );
+  images = blockText.filter( blockFilter );
+
+  images.forEach( function( img ) {
+    var inner;
+
+    if ( options.parseInternalMarkdown ) {
+      inner = parseMarkdown( img.trim() ).replace( /<p>|<\/p>/g, "" );
+    } else {
+      img = img.trim().replace( /\\_/g, "_" );
+      inner = '<img src="' + options.baseImageUrl + img + '" alt="" />';
+    }
+
+    figures.push(
+      '<div>' +
+      inner +
+      '</div>'
+    );
+  });
+
+  return;
+}
+
+var Flexbox = function(block, baseImageUrl, parseInternalMarkdown) {
+  console.log('Flexbox');
+
+  options.baseImageUrl = baseImageUrl || '';
+  options.parseInternalMarkdown = parseInternalMarkdown || true;
+
+  buildFigures(block);
+
+  var output = ( '<div class="figure-block">' +
+    figures.join( '\n' )
+    + '</div>'
+  );
+
+  return;
+};
+
+var Tables = function(block, baseImageUrl, parseInternalMarkdown) {
+  // not implemented
+};
+
+module.exports = {
+  Flexbox: Flexbox,
+  Tables: Tables
+};
--- src/index.js
+++ src/index.js
@@ -1,69 +1,20 @@
 'use strict';
-
-var MarkupIt = require( 'markup-it' );
-var markdownSyntax = require( 'markup-it/syntaxes/markdown' );
-var htmlSyntax = require( 'markup-it/syntaxes/html' );
-
-var markdown = new MarkupIt( markdownSyntax );
-var html = new MarkupIt( htmlSyntax );
-
-function parseMarkdown( text ) {
-  md = markdown.toContent( text );
-  parsed = html.toText( md );
-  return parsed;
-};
-
-function blockFilter( imageItem ) {
-  var image = imageItem.trim()
-  return image.length > 1;
-}
-
-function flexWrap( block, config ) {
-  var blockText, images, figures = [];
-
-  blockText = block.body.split( /\r?\n/ );
-  images = blockText.filter( blockFilter );
-
-  images.forEach( function( img ) {
-    var inner = "";
-
-    if ( config.parseInternalMarkdown ) {
-      inner = parseMarkdown( img.trim() ).replace( /<p>|<\/p>/g, "" );
-    } else {
-      img = img.trim().replace( /\\_/g, "_" );
-      inner = '<img src="' + config.baseImageUrl + img + '" alt="" />';
-    }
-
-    figures.push(
-      '<div>' +
-      inner +
-      '</div>' );
-  } );
-
-  var body = ( '<div class="figure-block">' +
-    figures.join( '\n' ) +
-    '</div>' );
-
-  return {
-    body: body,
-    parse: true
-  };
-}
-
 module.exports = {
-  website: {
+  book: {
     assets: './assets',
-    css: [ 'plugin.css' ]
-  },
-  ebook: {
-    assets: './assets',
-    css: [ 'plugin.css' ]
+    css: [ 'plugin.css' ],
+    js: [ 'plugin.js' ]
   },
   blocks: {
     fleximages: {
       process: function( block ) {
+        var Plugin = require('../assets/plugin.js')
         var config = this.options.pluginsConfig.flexImages || {};
-        return flexWrap( block, config )
+        var flexbox = new Plugin.Flexbox(block, config);
+
+        console.log(flexbox.output); // TODO:0 remove log comments
+
+        return 'some image block';
       }
     }
   }
